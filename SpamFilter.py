5e733f4c144d6c0f152bb3e2261e7c87de44514a
alachhman.github.io <alachhman@users.noreply.github.com>
2018-11-13 17:54:24 -0500

    #3
    Added rudimentary spam v not_spam classifier

--- SpamFilter.py
+++ SpamFilter.py
@@ -2,9 +2,25 @@ from sklearn import svm
 
 
 def clf_example():
-
-    x = [[0, 0], [1, 1]]
-    y = [0, 1]
-    clf = svm.SVC(gamma='scale')
+    x = [[1], [2]]
+    y = ["spam", "not_spam"]
+    clf = svm.SVC(gamma='scale', decision_function_shape='ovo')
     clf.fit(x, y)
-    clf.predict([[2., 2.]])
+    print(clf.support_vectors_)
+    print(clf.predict([[4]]))
+    #dec = clf.decision_function([[1]])
+    #dec.shape[1]
+    #clf.decision_function_shape = "ovr"
+    #dec = clf.decision_function([[1]])
+    #print(dec.shape[1])
+
+
+def main():
+    clf_example()
+
+
+main()
+
+
+
+
