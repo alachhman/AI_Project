be386613f7377d0669dc35aa9c4903a9ff3f8d19
alachhman.github.io <alachhman@users.noreply.github.com>
2018-11-13 19:54:29 -0500

    #4
    Comment Cleanup
    Added downstream necessary imports
    Cleaned map initialization

--- SpamFilter.py
+++ SpamFilter.py
@@ -1,18 +1,16 @@
-from sklearn import svm
+import os
+import numpy as np
+from collections import Counter
+from sklearn.naive_bayes import MultinomialNB, GaussianNB, BernoulliNB
+from sklearn.svm import SVC, NuSVC, LinearSVC
 
 
 def clf_example():
     x = [[1], [2]]
     y = ["spam", "not_spam"]
-    clf = svm.SVC(gamma='scale', decision_function_shape='ovo')
+    clf = MultinomialNB()
     clf.fit(x, y)
-    print(clf.support_vectors_)
     print(clf.predict([[4]]))
-    #dec = clf.decision_function([[1]])
-    #dec.shape[1]
-    #clf.decision_function_shape = "ovr"
-    #dec = clf.decision_function([[1]])
-    #print(dec.shape[1])
 
 
 def main():
